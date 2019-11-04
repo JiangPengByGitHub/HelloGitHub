6651052557e329679e7f28fa592ae5f73505da01
JiangPengByGitHub <30764805+JiangPengByGitHub@users.noreply.github.com>
2019-11-04 21:17:13 +0800

    又一次提交

--- Text/src/org/jiangpeng/test/TestClass.java
+++ Text/src/org/jiangpeng/test/TestClass.java
@@ -4,5 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("hellow word!");
+		System.out.println("hellow word!");
+		System.out.println("hellow word!");
+		System.out.println("hellow word!");
 	}
 }
